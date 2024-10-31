7f585e25e605ff4f716732ad604e5d101e359c39
wamitinewton <wamitinewton@gmail.com>
2024-10-31 10:35:21 +0300

    adaptive theme and error animations

--- app/src/main/java/com/example/mustmarket/core/SharedComposables/ErrorState.kt
+++ app/src/main/java/com/example/mustmarket/core/SharedComposables/ErrorState.kt
@@ -1,5 +1,14 @@
 package com.example.mustmarket.core.SharedComposables
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,31 +18,78 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
- fun ErrorState() {
-    Box(
-        modifier = Modifier
-            .height(90.dp)
-            .fillMaxWidth()
-            .padding(horizontal = 20.dp)
-            .border(
-                width = 1.dp, color = Color.White,
-                shape = RoundedCornerShape(4.dp)
+fun ErrorState(
+    modifier: Modifier = Modifier,
+    visible: Boolean = true
+) {
+    AnimatedErrorEntrance(
+        visible = visible,
+        modifier = modifier
+    ) {
+        Box(
+            modifier = Modifier
+                .height(90.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .border(
+                    width = 1.dp, color = Color.White,
+                    shape = RoundedCornerShape(4.dp)
+                ),
+            contentAlignment = Alignment.Center,
+
+
+            ) {
+            Text(
+                text = "Server error. Try again later",
+                color = MaterialTheme.colors.error,
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun AnimatedErrorEntrance(
+    visible: Boolean = true,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    val transitionState = remember {
+        MutableTransitionState(false).apply {
+            targetState = visible
+        }
+    }
+    AnimatedVisibility(
+        visibleState = transitionState,
+        modifier = modifier,
+        enter = fadeIn(
+            animationSpec = tween(durationMillis = 500)
+        ) + slideInVertically(
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioHighBouncy,
+                stiffness = Spring.StiffnessLow
             ),
-        contentAlignment = Alignment.Center,
-
-
-        ) {
-        Text(
-            text = "Server error. Try again later",
-            color = MaterialTheme.colors.error,
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.padding(16.dp)
+            initialOffsetY = { fullHeight -> -fullHeight / 3 }
+        ),
+        exit = fadeOut(
+            animationSpec = tween(durationMillis = 500)
+        ) + slideOutVertically(
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioHighBouncy,
+                stiffness = Spring.StiffnessVeryLow
+            ),
+            targetOffsetY = { fullHeight -> -fullHeight / 3 }
         )
+    ) {
+        content()
     }
 }
