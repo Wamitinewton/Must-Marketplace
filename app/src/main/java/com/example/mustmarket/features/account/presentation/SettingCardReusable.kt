90887d0a98877e3867017f698721ef5050981431
Wamitinewton <wamitinewton@gmail.com>
2024-11-21 10:26:13 +0300

    account screen theming

--- app/src/main/java/com/example/mustmarket/features/account/presentation/SettingCardReusable.kt
+++ app/src/main/java/com/example/mustmarket/features/account/presentation/SettingCardReusable.kt
@@ -8,9 +8,12 @@ import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.mustmarket.features.account.domain.models.SettingItem
 import com.example.mustmarket.ui.theme.Shapes
+import com.example.mustmarket.ui.theme.ThemeUtils
+import com.example.mustmarket.ui.theme.ThemeUtils.themed
 import com.example.mustmarket.ui.theme.gray01
 
 @Composable
--- app/src/main/java/com/example/mustmarket/features/product/presentation/shop/ShopScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mustmarket.features.product.presentation.shop
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun ShopScreen(modifier: Modifier = Modifier) {
-    
-}
